040f3a6c618033e836a7e1f46acb8ec2fe84d318
lesto <you@example.com>
2021-10-11 15:48:52 +0200

    added new unit test

--- src/test_intel.py
+++ src/test_intel.py
@@ -66,6 +66,39 @@ class TestIntelParser(unittest.TestCase):
         
         self.assertEqual(expected, allGpu)
         
+    def test_txt3(self):
+        self.maxDiff = None
+        allGpu = {}
+        mutex = threading.Lock()
+        parser = intel.Intel(allGpu, mutex)
+        with open('../test_files/intel_gpu_output_3.txt', 'r') as file1:
+            for line in file1.readlines():
+                parser.parseLine(line)
+            
+        expected = {}
+        expected['Intel.0.fReq'] = '217'
+        expected['Intel.0.fAtt'] = '217'
+        expected['Intel.0.irq/s'] = '590'
+        expected['Intel.0.rc6.%'] = '51'
+        expected['Intel.0.gpu'] = '0.34'
+        expected['Intel.0.pkg'] = '5.34'
+        expected['Intel.0.rd'] = '4881'
+        expected['Intel.0.wr'] = '1905'
+        expected['Intel.0.RCS.%'] = '28.76'
+        expected['Intel.0.RCS.se'] = '0'
+        expected['Intel.0.RCS.wa'] = '0'
+        expected['Intel.0.BCS.%'] = '0.00'
+        expected['Intel.0.BCS.se'] = '0'
+        expected['Intel.0.BCS.wa'] = '0'
+        expected['Intel.0.VCS.%'] = '0.00'
+        expected['Intel.0.VCS.se'] = '0'
+        expected['Intel.0.VCS.wa'] = '0'
+        expected['Intel.0.VECS.%'] = '0.00'
+        expected['Intel.0.VECS.se'] = '0'
+        expected['Intel.0.VECS.wa'] = '0'
+        
+        self.assertEqual(expected, allGpu)
+        
 
 if __name__ == '__main__':
     unittest.main()
