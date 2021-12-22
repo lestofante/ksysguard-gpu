78634e75f556bbd454320d12226eacbe5827745f
Louis Personnaz <louis.personnaz@telecom-paris.fr>
2021-12-22 12:11:17 +0100

    install.sh: remove zip archive after copy

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-echo "Coping service and executable"
+echo "Copying service and executable"
 cp systemd-unit/ksysguard-gpu.service /etc/systemd/system/ksysguard-gpu.service
 cd src
 zip -r ksysguard-gpu.zip __main__.py amd.py intel.py nvidia.py ksysguard-gpu.py
 cp ksysguard-gpu.zip /usr/bin/
+rm ksysguard-gpu.zip
 
 echo "Enabling ksysguard-gpu in systemd"
 systemctl enable --now ksysguard-gpu
+
