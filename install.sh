c1b7705d5f24d2e22064728fd8e2ea1568d8c5fa
Stehlampe2020 <94976382+Stehlampe2020@users.noreply.github.com>
2023-12-02 17:25:17 +0100

    Fix typo in install.sh
    "Coping" → "Copying"

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "Coping service and executable"
+echo "Copying service and executable"
 cp systemd-unit/ksysguard-gpu.service /etc/systemd/system/ksysguard-gpu.service
 cp src/ksysguard-gpu.py /usr/bin/
 chmod +x /usr/bin/ksysguard-gpu.py
